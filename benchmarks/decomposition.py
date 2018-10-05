c668a4cbde6abf6fcdda58ad62186b9d6ce756d4
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2018-10-05 17:01:07 +0200

    add pca et neighbors

---
+++ benchmarks/decomposition.py
@@ -0,0 +1,32 @@
+from sklearn.decomposition import PCA
+
+from .common import Benchmark
+from .datasets import _mnist_dataset
+
+
+class PCA_(Benchmark):
+    """
+    Benchmarks for PCA.
+    """
+    # params = (svd_solver)
+    param_names = ['params']
+    params = ([('full',),
+               ('arpack',),
+               ('randomized',)],)
+
+    def setup(self, params):
+        svd_solver = params[0]
+
+        self.X, _ = _mnist_dataset()
+
+        self.pca_params = {'n_components': 8,
+                           'svd_solver': svd_solver,
+                           'random_state': 0}
+
+    def time_fit(self, *args):
+        pca = PCA(**self.pca_params)
+        pca.fit(self.X)
+
+    def peakmem_fit(self, *args):
+        pca = PCA(**self.pca_params)
+        pca.fit(self.X)
---
+++ benchmarks/neighbors.py
@@ -0,0 +1,35 @@
+from sklearn.neighbors import KNeighborsClassifier
+
+from .common import Benchmark
+from .datasets import _20newsgroups_lowdim_dataset
+
+
+class KNeighborsClassifier_(Benchmark):
+    """
+    Benchmarks for KNeighborsClassifier.
+    """
+    # params = (algorithm)
+    param_names = ['params'] + Benchmark.param_names
+    params = ([('brute',),
+               ('kd_tree',),
+               ('ball_tree',)],) + Benchmark.params
+
+    def setup(self, params, *common):
+        algo = params[0]
+
+        n_jobs = common[0]
+
+        self.X, self.y = _20newsgroups_lowdim_dataset()
+
+        self.knn_params = {'algorithm': algo,
+                           'n_jobs': n_jobs}
+
+    def time_fit_predict(self, *args):
+        knn = KNeighborsClassifier(**self.knn_params)
+        knn.fit(self.X, self.y)
+        knn.predict(self.X)
+
+    def peakmem_fit_predict(self, *args):
+        knn = KNeighborsClassifier(**self.knn_params)
+        knn.fit(self.X, self.y)
+        knn.predict(self.X)
