31e18f6fca106aba463a94e3d13f3e910b2101c1
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2018-10-09 10:58:47 +0200

    update datasets

--- benchmarks/datasets.py
+++ benchmarks/datasets.py
@@ -1,10 +1,11 @@
 import numpy as np
+import scipy.sparse as sp
 
 from joblib import Memory
 
 from sklearn.decomposition import TruncatedSVD
 from sklearn.datasets import (load_sample_image, fetch_20newsgroups_vectorized,
-                              fetch_openml, load_digits)
+                              fetch_openml, load_digits, make_regression)
 from sklearn.preprocessing import StandardScaler, MaxAbsScaler
 
 
@@ -51,4 +52,22 @@ def _digits_dataset(dtype=np.float32):
     X, y = load_digits(return_X_y=True)
     X = X.astype(dtype, copy=False)
     X = StandardScaler().fit_transform(X)
+    return X, y
+
+
+@M.cache
+def _synth_regression_lowdim_dataset(n_samples=1000000, n_features=100,
+                                     dtype=np.float32):
+    X, y = make_regression(n_samples=n_samples, n_features=n_features)
+    X = X.astype(dtype, copy=False)
+    X = StandardScaler().fit_transform(X)
+    return X, y
+
+
+@M.cache
+def _synth_regression_highdim_dataset(n_samples=10000, n_features=100000,
+                                      dtype=np.float32):
+    X = sp.random(n_samples, n_features, density=0.001, format='csr',
+                  dtype=dtype)
+    y = np.random.rand(n_samples)
     return X, y
