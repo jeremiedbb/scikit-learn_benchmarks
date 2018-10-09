84f9492b0da2e585500fa1e16579c859e24fbd3d
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2018-10-09 18:30:20 +0200

    better datasets

--- benchmarks/datasets.py
+++ benchmarks/datasets.py
@@ -4,9 +4,11 @@ import scipy.sparse as sp
 from joblib import Memory
 
 from sklearn.decomposition import TruncatedSVD
-from sklearn.datasets import (load_sample_image, fetch_20newsgroups_vectorized,
-                              fetch_openml, load_digits, make_regression)
-from sklearn.preprocessing import StandardScaler, MaxAbsScaler
+from sklearn.datasets import (load_sample_image, fetch_openml,
+                              fetch_20newsgroups, load_digits,
+                              make_regression)
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 # memory location for caching datasets
@@ -23,19 +25,23 @@ def _china_dataset(dtype=np.float32):
 
 @M.cache
 def _20newsgroups_highdim_dataset(dtype=np.float32):
-    X, y = fetch_20newsgroups_vectorized(return_X_y=True)
+    newsgroups = fetch_20newsgroups()
+    vectorizer = TfidfVectorizer(min_df=2, max_df=0.9)
+    X = vectorizer.fit_transform(newsgroups.data)
     X = X.astype(dtype, copy=False)
-    X = MaxAbsScaler().fit_transform(X)
+    y = newsgroups.target
     return X, y
 
 
 @M.cache
-def _20newsgroups_lowdim_dataset(dtype=np.float32):
-    X, y = fetch_20newsgroups_vectorized(return_X_y=True)
+def _20newsgroups_lowdim_dataset(n_components=100, dtype=np.float32):
+    newsgroups = fetch_20newsgroups()
+    vectorizer = TfidfVectorizer(min_df=2, max_df=0.9)
+    X = vectorizer.fit_transform(newsgroups.data)
     X = X.astype(dtype, copy=False)
-    X = MaxAbsScaler().fit_transform(X)
-    svd = TruncatedSVD(n_components=10)
+    svd = TruncatedSVD(n_components=n_components)
     X = svd.fit_transform(X)
+    y = newsgroups.target
     return X, y
 
 
@@ -43,7 +49,7 @@ def _20newsgroups_lowdim_dataset(dtype=np.float32):
 def _mnist_dataset(dtype=np.float32):
     X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
     X = X.astype(dtype, copy=False)
-    X = StandardScaler().fit_transform(X)
+    X = MaxAbsScaler().fit_transform(X)
     return X, y
 
 
@@ -51,23 +57,19 @@ def _mnist_dataset(dtype=np.float32):
 def _digits_dataset(dtype=np.float32):
     X, y = load_digits(return_X_y=True)
     X = X.astype(dtype, copy=False)
-    X = StandardScaler().fit_transform(X)
+    X = MaxAbsScaler().fit_transform(X)
     return X, y
 
 
 @M.cache
-def _synth_regression_lowdim_dataset(n_samples=1000000, n_features=100,
-                                     dtype=np.float32):
-    X, y = make_regression(n_samples=n_samples, n_features=n_features)
+def _synth_regression_dataset(n_samples=1000, n_features=10000,
+                              representation='dense', dtype=np.float32):
+    X, y = make_regression(n_samples=n_samples, n_features=n_features,
+                           n_informative=n_features//10, noise=0.1)
     X = X.astype(dtype, copy=False)
-    X = StandardScaler().fit_transform(X)
-    return X, y
 
+    if representation is 'sparse':
+        X[X < 2] = 0
+        X = sp.csr_matrix(X)
 
-@M.cache
-def _synth_regression_highdim_dataset(n_samples=10000, n_features=100000,
-                                      dtype=np.float32):
-    X = sp.random(n_samples, n_features, density=0.001, format='csr',
-                  dtype=dtype)
-    y = np.random.rand(n_samples)
     return X, y
