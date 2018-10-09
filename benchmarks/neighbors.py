4088e74c712ab01329a870f5e258082f9e738475
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2018-10-09 18:31:33 +0200

    knn hilo param

--- benchmarks/neighbors.py
+++ benchmarks/neighbors.py
@@ -8,18 +8,25 @@ class KNeighborsClassifier_bench(Benchmark):
     """
     Benchmarks for KNeighborsClassifier.
     """
-    # params = (algorithm)
+    # params = (algorithm, wideness)
     param_names = ['params'] + Benchmark.param_names
-    params = ([('brute',),
-               ('kd_tree',),
-               ('ball_tree',)],) + Benchmark.params
+    params = ([('brute', 'high_dim'),
+               ('brute', 'low_dim'),
+               ('kd_tree', 'high_dim'),
+               ('kd_tree', 'low_dim'),
+               ('ball_tree', 'high_dim'),
+               ('ball_tree', 'low_dim')],) + Benchmark.params
 
     def setup(self, params, *common):
         algo = params[0]
+        wideness = params[1]
 
         n_jobs = common[0]
 
-        self.X, self.y = _20newsgroups_lowdim_dataset()
+        if wideness is 'high_dim':
+            self.X, self.y = _20newsgroups_lowdim_dataset(n_components=50)
+        else:
+            self.X, self.y = _20newsgroups_lowdim_dataset(n_components=10)
 
         self.knn_params = {'algorithm': algo,
                            'n_jobs': n_jobs}
