e474b349baa1ee37f2959f67c6f4b8e823d95a73
Dmitry Temnov <temnov.dmitry@gmail.com>
2018-10-30 17:53:14 +0300

    Some organizational fixes

--- benchmarks/crossvalidation.py
+++ benchmarks/crossvalidation.py
@@ -7,26 +7,26 @@ from .datasets import _synth_classification_dataset
 
 
 class CrossValidationSuite(Benchmark):
-
     def setup(self, params):
-        X, y = _synth_classification_dataset(
+        self.X, self.y = _synth_classification_dataset(
             n_samples=70000, n_features=200, random_state=0
         )
 
         self.clf = RandomForestClassifier(n_estimators=100, random_state=0)
 
+    def time_crossval(self, params):
+        cross_val_score(
+            self.clf, self.X, self.y, n_jobs=params, cv=4
+        )
+
+    def time_gridsearch(self, params):
         # Setup for GridSearchCV
-        parameters = {
+        gs_parameters = {
             "n_estimators": [10, 50, 100, 500],
             "max_depth": [5, 10, None],
             "max_features": [0.1, 0.5, 0.8, 1.0],
         }
-        self.gs_clf = GridSearchCV(
-            RandomForestClassifier(), parameters, cv=3, n_jobs=params
+        gs_clf = GridSearchCV(
+            RandomForestClassifier(), gs_parameters, cv=3, n_jobs=params
         )
-
-    def time_crossval(self, params):
-        cross_val_score(self.clf, self.X_train, self.y_train, n_jobs=params, cv=4)
-
-    def time_gridsearch(self, params):
-        self.gs_clf.fit(self.X_train, self.y_train)
+        gs_clf.fit(self.X, self.y)
