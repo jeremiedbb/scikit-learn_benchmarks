dbad4eedfe07f8610620cf917108e69677fd1189
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2018-12-17 17:22:52 +0100

    svm

--- benchmarks/svm.py
+++ benchmarks/svm.py
@@ -14,25 +14,24 @@ def get_optimal_cache_size(n_features, dtype=np.float32):
 
 
 class SVC_bench(Benchmark):
+    """Benchmarks for SVC."""
 
-    # params = (Kernel)
-    param_names = ['params']
-    params = ([('linear',),
-               ('poly',),
-               ('rbf',),
-               ('sigmoid',)],)
+    param_names = ['kernel']
+    params = (['linear', 'poly', 'rbf', 'sigmoid'],)
 
-    def setup(self, params):
+    def setup(self, *params):
+        kernel, = params
+
         self.X, self.y = _synth_classification_dataset()
         self.cache_size = get_optimal_cache_size(self.X.shape[1])
 
-        self.svc_params = {"C": 0.01,
-                           "cache_size": self.cache_size,
-                           "max_iter": 100,
-                           "tol": 1e-16,
-                           "kernel": params[0],
-                           "random_state": 42,
-                           "shrinking": True}
+        self.svc_params = {'C': 0.01,
+                           'cache_size': self.cache_size,
+                           'max_iter': 100,
+                           'tol': 1e-16,
+                           'kernel': kernel,
+                           'random_state': 42,
+                           'shrinking': True}
 
     def time_fit(self, *args):
         svc = svm.SVC(**self.svc_params)
