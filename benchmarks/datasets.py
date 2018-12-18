9619811d1a6027ee629968b5a8bccae7a2f87fc2
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2018-12-18 17:08:27 +0100

    datasets

--- benchmarks/datasets.py
+++ benchmarks/datasets.py
@@ -6,7 +6,7 @@ from sklearn.decomposition import TruncatedSVD
 from sklearn.datasets import (load_sample_image, fetch_20newsgroups,
                               fetch_openml, load_digits, make_regression,
                               make_classification, fetch_olivetti_faces)
-from sklearn.preprocessing import MaxAbsScaler
+from sklearn.preprocessing import MaxAbsScaler, StandardScaler
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # memory location for caching datasets
@@ -14,20 +14,21 @@ M = Memory(location='/tmp/joblib')
 
 
 @M.cache
-def _china_dataset(dtype=np.float32):
+def _china_dataset(n_samples=None, dtype=np.float32):
     img = load_sample_image('china.jpg')
     X = np.array(img, dtype=dtype) / 255
-    X = X.reshape((-1, 3))
+    X = X.reshape((-1, 3))[:n_samples]
     return X
 
 
 @M.cache
-def _20newsgroups_highdim_dataset(dtype=np.float32):
+def _20newsgroups_highdim_dataset(n_samples=None, ngrams=(1, 1),
+                                  dtype=np.float32):
     newsgroups = fetch_20newsgroups()
-    vectorizer = TfidfVectorizer(min_df=2, max_df=0.9)
-    X = vectorizer.fit_transform(newsgroups.data)
+    vectorizer = TfidfVectorizer(min_df=2, max_df=0.9, ngram_range=ngrams)
+    X = vectorizer.fit_transform(newsgroups.data[:n_samples])
     X = X.astype(dtype, copy=False)
-    y = newsgroups.target
+    y = newsgroups.target[:n_samples]
     return X, y
 
 
@@ -52,40 +53,32 @@ def _mnist_dataset(dtype=np.float32):
 
 
 @M.cache
-def _digits_dataset(dtype=np.float32):
+def _digits_dataset(n_samples=None, dtype=np.float32):
     X, y = load_digits(return_X_y=True)
     X = X.astype(dtype, copy=False)
     X = MaxAbsScaler().fit_transform(X)
-    return X, y
+    return X[:n_samples], y[:n_samples]
 
 
 @M.cache
 def _synth_regression_dataset(n_samples=1000, n_features=10000,
-                              representation='dense', dtype=np.float32):
+                              dtype=np.float32):
     X, y = make_regression(n_samples=n_samples, n_features=n_features,
-                           n_informative=n_features // 10, noise=0.1)
+                           n_informative=n_features // 10, noise=0.1,
+                           random_state=0)
     X = X.astype(dtype, copy=False)
-
-    if representation is 'sparse':
-        X[X < 2] = 0
-        X = sp.csr_matrix(X)
-
+    X = StandardScaler().fit_transform(X)
     return X, y
 
 
 @M.cache
 def _synth_classification_dataset(n_samples=1000, n_features=10000,
-                                  representation='dense', n_classes=2,
-                                  dtype=np.float32):
-
+                                  n_classes=2, dtype=np.float32):
     X, y = make_classification(n_samples=n_samples, n_features=n_features,
                                n_classes=n_classes, random_state=42,
                                n_informative=n_features, n_redundant=0)
     X = X.astype(dtype, copy=False)
-    if representation is 'sparse':
-        X[X < 2] = 0
-        X = sp.csr_matrix(X)
-    X = MaxAbsScaler().fit_transform(X)
+    X = StandardScaler().fit_transform(X)
     return X, y
 
 
