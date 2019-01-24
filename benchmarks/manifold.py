ca1bb2a8a78cca41ec75b5c9979dd016e72703e3
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2019-01-24 17:02:43 +0100

    manifold

--- benchmarks/manifold.py
+++ benchmarks/manifold.py
@@ -17,20 +17,8 @@ class TSNE_bench(Benchmark):
 
         n_samples = 500 if method == 'exact' else None
 
-        self.X, _ = _digits_dataset(n_samples=n_samples)
+        self.X, _, _, _ = _digits_dataset(n_samples=n_samples)
 
-        self.tsne_params = {'random_state': 0,
-                            'method': method}
+        self.estimator = TSNE(random_state=0, method=method)
 
-    def time_fit(self, *args):
-        tsne = TSNE(**self.tsne_params)
-        tsne.fit(self.X)
-
-    def peakmem_fit(self, *args):
-        tsne = TSNE(**self.tsne_params)
-        tsne.fit(self.X)
-
-    def track_fit(self, *args):
-        tsne = TSNE(**self.tsne_params)
-        tsne.fit(self.X)
-        return tsne.n_iter_
+        self.estimator.fit(self.X)
